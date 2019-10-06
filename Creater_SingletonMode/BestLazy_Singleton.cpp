a017e5d3750ea8fc60e925ade5e9d8a65bb2b5b5
ZachZheng <1664844655@qq.com>
2019-10-06 17:39:21 +0800

    创建型--单例模式

---
+++ Creater_SingletonMode/BestLazy_Singleton.cpp
@@ -0,0 +1,29 @@
+#includes <iostream>
+
+class Singleton {
+private:
+	Singleton() {
+		std::cout << "constructor called!" << std::endl;
+	}
+	
+public:
+	~Singleton() {
+		std::cout << "Destructor called!" << std::endl;
+	}
+	
+	/*禁止复制和拷贝*/
+	Singleton(const Singleton &) = delete;
+	Singleton & operator=(const Singleton &) = delete;
+	
+	/*静态成员属性*/
+	static Singleton & get_instance() {
+		static Singleton instance;
+		return instance;
+	}
+};
+
+int main()
+{
+	Singleton & instance_1 = Singleton::get_instance();
+	Singleton & instance_2 = Singleton::get_instance();
+}
---
+++ Creater_SingletonMode/DefectLazy_Singleton.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+// version1:
+// with problems below:
+// 1. thread is not safe
+// 2. memory leak
+
+class Singleton{
+private:
+    /*构造函数私有化*/
+    Singleton() { std::cout<<"constructor called!"<<std::endl; }
+
+    /*禁止赋值和拷贝*/
+    Singleton(Singleton&)=delete;
+    Singleton& operator=(const Singleton&)=delete;
+
+    /*静态成员*/
+    static Singleton* m_instance_ptr;
+public:
+    ~Singleton() { std::cout<<"destructor called!"<<std::endl; }
+
+    /*通过接口获取实例*/
+    static Singleton* get_instance() {
+        if(m_instance_ptr==nullptr)  
+            m_instance_ptr = new Singleton;
+        return m_instance_ptr;
+    }
+    void use() const { std::cout << "in use" << std::endl; }
+};
+
+/*类静态成员初始化*/
+Singleton* Singleton::m_instance_ptr = nullptr;
+
+int main(){
+    Singleton* instance = Singleton::get_instance();
+    Singleton* instance_2 = Singleton::get_instance();
+    return 0;
+}
